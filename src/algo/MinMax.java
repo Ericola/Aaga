f94339a88a4693eb9d26932f264762a7031a185f
LorantK <kevin.lorant@gmail.com>
2015-12-14 11:51:26 +0100

    MinMax

--- src/algo/MinMax.java
+++ src/algo/MinMax.java
@@ -2,12 +2,42 @@ package algo;
 
 public class MinMax {
 
+	
+	private int min;
+	private int max;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+	}
+	
+	public void naive(int[] tab) {
+		min = max = tab[0];
+		for(int i = 1; i<tab.length;i++){
+			if(tab[i]<min){
+				min = tab[i];
+			}
+			if(tab[i]>max){
+				max = tab[i];
+			}
+		}
+	}
+	
+	public void opti(int[] tab){
+		min = max = tab[tab.length - 1];
+		for(int i=0;i<tab.length - 1;i +=2){
+			if(tab[i] < tab[i+1]){
+				if(tab[i]<min) min = tab[i];
+				if(tab[i+1]>max) max = tab[i+1];
+			}
+			else {
+				if(tab[i+1]<min) min = tab[i+1];
+				if(tab[i]>max) max = tab[i];
+			}
+		}
 	}
 
 }

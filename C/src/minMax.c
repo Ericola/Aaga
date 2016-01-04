e3a6745bb4cc14f9c261e98332e59b7594be8c10
Ericola <elimao1993@gmail.com>
2016-01-04 10:15:56 +0100

    merge

--- C/src/minMax.c
+++ C/src/minMax.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+clock_t debut, fin;
+double tps_ecoule;
 
 void minMax(float tab[], int n, float* max, float* min){
   int i = 0;
@@ -18,8 +21,10 @@ int main(int argc, char** argv){
   float tab[] = {0.0, 0.1, 0.9};
   float max = 0.0;
   float min = 0.0;
+  debut = clock();
   minMax(tab, 3, &max, &min);
-  printf("min : %f \n max : %f \n", min, max);
+  fin = clock();
+  printf("min : %f \n max : %f \n Temps ecoule : %f \n", min, max, (double)(fin - debut) / CLOCKS_PER_SEC);
 
   return 0;
 }

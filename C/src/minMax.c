4efb5899fee76be054ad80169ecf3d3fe42d3a21
LorantK <kevin.lorant@gmail.com>
2016-01-04 12:18:52 +0100

    Algo comparaison minmax C

--- C/src/minMax.c
+++ C/src/minMax.c
@@ -38,20 +38,64 @@ void optiMinMax(float tab[], int n, float* max, float* min) {
       }
     }
   }
-
 } 
 
-int main(int argc, char** argv){
+void testNaif() {
   float tab[] = {0.0, 0.1, 0.9};
-  /*float max = tab[0];
+  int n = 3;
+  float max = tab[0];
   float min = tab[0];
-  naiveminMax(tab, n, &max, &min);*/
+  debut = clock();
+  naiveminMax(tab, n, &max, &min);
+  fin = clock();
+  printf("min : %f \n max : %f \n Temps ecoule : %f \n", min, max, (double)(fin - debut) / CLOCKS_PER_SEC);
+}
+
+void testOpti() {
+  float tab[] = {0.0, 0.1, 0.9};
+  int n = 3; 
   float max = tab[n-1];
   float min = tab[n-1];
   debut = clock();
   optiMinMax(tab,n,&max,&min);
   fin = clock();
   printf("min : %f \n max : %f \n Temps ecoule : %f \n", min, max, (double)(fin - debut) / CLOCKS_PER_SEC);
+}
 
+
+void comparatif() {
+  int i;
+  int j;
+  float max;
+  float min;
+  double dur1;
+  double dur2;
+  FILE* fichier = fopen("courbe.txt","w+");
+  for(j = 1;j<100000000;j+=1000000) {
+    printf("%d \n",j);
+    float tab[j];
+    for(i = 0;i<j;i++) {
+      srand(time(NULL)); 
+      tab[i] = (float)rand()/(float)RAND_MAX;
+    }
+    max = tab[0];
+    min = tab[0];
+    debut = clock();
+    naiveminMax(tab,j,&max,&min);
+    fin = clock();
+    dur1 = (double)(fin-debut)/CLOCKS_PER_SEC;
+    max = tab[j-1];
+    min = tab[j-1];
+    debut = clock();
+    optiMinMax(tab,j,&max,&min);
+    fin = clock();
+    dur2 = (double)(fin-debut)/CLOCKS_PER_SEC;
+    fprintf(fichier,"%d %f %f",j,dur1,dur2);
+  }
+  fclose(fichier);
+}
+
+int main(int argc, char** argv){
+  comparatif();
   return 0;
 }
